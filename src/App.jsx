d61e0b01784eae1d81538b32c04d65bf5b570fbf
duddk <duddk@scatterx.io>
2024-09-23 15:18:15 +0900

    link > useNavigate로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Users from './Users';
@@ -16,10 +16,13 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
 
-function App() {
+function NavigationMenu() {
   const [navigation, setNavigation] = useState(initialNavigation);
+  const navigate = useNavigate();
 
   const onClick = (selectedItem) => {
+    navigate(selectedItem.href); 
+
     setNavigation((prevNavigation) =>
       prevNavigation.map(item =>
         item.name === selectedItem.name 
@@ -30,26 +33,32 @@ function App() {
   };
 
   return (
+    <div className="bg-lime-900 p-4">
+      <ul className="flex space-x-4">
+        {navigation.map((item) => (
+          <li
+            onClick={() => onClick(item)} 
+            key={item.name}
+            aria-current={item.current ? 'page' : undefined}
+            className={classNames(
+              item.current
+                ? 'bg-lime-950 text-white'
+                : 'text-gray-300 hover:bg-lime-950 hover:text-white',
+              'rounded-md px-3 py-2 text-sm font-medium cursor-pointer'
+            )}
+          >
+            {item.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function App() {
+  return (
     <Router>
-      <div className="bg-lime-900 p-4">
-        <ul className="flex space-x-4">
-          {navigation.map((item) => (
-            <li
-              onClick={() => onClick(item)}
-              key={item.name}
-              aria-current={item.current ? 'page' : undefined}
-              className={classNames(
-                item.current
-                  ? 'bg-lime-950 text-white'
-                  : 'text-gray-300 hover:bg-lime-950 hover:text-white',
-                'rounded-md px-3 py-2 text-sm font-medium'
-              )}
-            >
-              <Link to={item.href}>{item.name}</Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <NavigationMenu />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
